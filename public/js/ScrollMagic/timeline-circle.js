dd9e1207e50e06e1046dc7885f362f397a716c69
Nils Schmidt <nils.schmidt@vinure.de>
2021-10-25 17:00:26 +0200

    footer updates

--- public/js/ScrollMagic/timeline-circle.js
+++ public/js/ScrollMagic/timeline-circle.js
@@ -4,7 +4,7 @@ var controller = new ScrollMagic.Controller();
 
     $(function () {
 
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 1300,})
+    var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 1050,})
     .setPin("#pin1")
     .addTo(controller);
 
